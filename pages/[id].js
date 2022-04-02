a2151e7fb90062847019b973b9968cc86b450ba2
Alayode Auroraixion <chris@kayode.me>
2022-04-02 15:34:43 -0400

    adding video view code snippet

--- pages/[id].js
+++ pages/[id].js
@@ -1,17 +1,35 @@
+import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabase";
+import Video from "react-player";
 
-const LessonDetails = ({ lessons }) => {
-  // console.log({ lessons });
+const LessonDetails = ({ lesson }) => {
+  const [videoUrl, setVideoUrl] = useState();
+
+  const getPremiumContent = async () => {
+    const { data } = await supabase
+      .from("premium_content")
+      .select("video_url")
+      .eq("id", lesson.id)
+      .single();
+
+    setVideoUrl(data?.video_url);
+  };
+
+  useEffect(() => {
+    getPremiumContent();
+  }, []);
+
   return (
-    <div className="w-full max-w-3x mx-auto py-16 px-8">
-      <h1 className="text-3xl mb-6">{lessons.titles}</h1>
-      <p>{lessons.description}</p>
+    <div className="w-full max-w-3xl mx-auto py-16 px-8">
+      <h1 className="text-3xl mb-6">{lesson.title}</h1>
+      <p>{lesson.description}</p>
+      {!!videoUrl && <Video url={videoUrl} width="100%" />}
     </div>
   );
-}; 
+};
 
 export const getStaticPaths = async () => {
-  const { data: lessons } = await supabase.from("lessons").select("id");
+  const { data: lessons } = await supabase.from("lesson").select("id");
 
   const paths = lessons.map(({ id }) => ({
     params: {
@@ -25,21 +43,18 @@ export const getStaticPaths = async () => {
   };
 };
 
-
 export const getStaticProps = async ({ params: { id } }) => {
-    const { data: lessons } = await supabase
-    .from("lessons")
+  const { data: lesson } = await supabase
+    .from("lesson")
     .select("*")
     .eq("id", id)
     .single();
-  
-    return {
-      props: {
-        lessons,
-      },
-    };
+
+  return {
+    props: {
+      lesson,
+    },
   };
-  
+};
 
 export default LessonDetails;
-
