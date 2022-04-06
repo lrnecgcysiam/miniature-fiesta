97ea87bb29ce30ce5ff46950ec7c78d64869efef
Alayode Auroraixion <chris@kayode.me>
2022-04-06 15:58:42 -0400

    adding pricing mocks

---
+++ pages/mocks/prices.mock.js
@@ -0,0 +1,40 @@
+const  PRICES_MOCK  = [
+    {
+      id: "price_1KlCoXFtWLQllTObHdl4c3YR",
+      "Product ID": "prod_LS72ZkqG4qnwG5",
+      name: "Pro Plan",
+      "Product Statement Descriptor": "",
+      "Product Tax Code": "txcd_10000000",
+      "Description": "",
+      "Created (UTC)": "2022-04-05 13:52",
+      price: 20000,
+      "Currency": "usd",
+      interval: "year",
+      "Interval Count": 1,
+      "Usage Type": "licensed",
+      "Aggregate Usage": "",
+      "Billing Scheme": "per_unit",
+      "Trial Period Days": "",
+      "Tax Behavior": "exclusive"
+    },
+    {
+      id: "price_1KlCnXFtWLQllTObSSvtvfF4",
+      "Product ID": "prod_LS71RhHrBi95Yj",
+      name: "Basic Plan",
+      "Product Statement Descriptor": "",
+      "Product Tax Code": "txcd_10000000",
+      "Description": "",
+      "Created (UTC)": "2022-04-05 13:51",
+      price: 2000,
+      "Currency": "usd",
+      interval: "month",
+      "Interval Count": 1,
+      "Usage Type": "licensed",
+      "Aggregate Usage": "",
+      "Billing Scheme": "per_unit",
+      "Trial Period Days": "",
+      "Tax Behavior": "exclusive"
+    }
+  ]
+
+  export default PRICES_MOCK
